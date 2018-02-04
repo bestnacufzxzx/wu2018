94058187128c7024f9bead71fd385a46d2cdc50d
DESKTOP-HILTF20\acer <bestnacufzxzx1@gmail.com>
2018-02-04 14:05:50 +0700

    TEst2

--- tests/AITest.php
+++ tests/AITest.php
@@ -6,14 +6,14 @@ final class AITest extends TestCase
 {
     public function testGender_Male(): void
     {
-        $result = AI::getGender('สวัสดีครับ');
+        $result = AI::getGender1('สวัสดีครับ');
         $expected_result = 'Male';
         $this->assertEquals($expected_result, $result);
     }
     
      public function testGender_Male(): void
     {
-        $result = AI::getGender('สวัสดีคราบ');
+        $result = AI::getGender2('สวัสดีคราบ');
         $expected_result = 'Male';
         $this->assertEquals($expected_result, $result);
     }
@@ -21,62 +21,62 @@ final class AITest extends TestCase
 
     public function testGender_Female(): void
     {
-        $result = AI::getGender('สวัสดีค่ะ');
+        $result = AI::getGender3('สวัสดีค่ะ');
         $expected_result = 'Female';
         $this->assertEquals($expected_result, $result);
     }
     public function testGender_Female(): void
     {
-        $result = AI::getGender('สวัสดีคะ');
+        $result = AI::getGender4('สวัสดีคะ');
         $expected_result = 'Female';
         $this->assertEquals($expected_result, $result);
     }
 
     public function testSentiment_Positive(): void
     {
-        $result = AI::Sentiment('สะบายดีไหม');
-        $expected_result = 'Positive';
-        $this->assertEquals($expected_result, $result);
-    }
-    
-    public function testSentiment_Positive(): void
-    {
-        $result = AI::Sentiment('ทำดีมากๆ');
+        $result = AI::Sentiment1('สะบายดีไหม');
         $expected_result = 'Positive';
         $this->assertEquals($expected_result, $result);
     }
 
     public function testSentiment_Positive(): void
     {
-        $result = AI::Sentiment('ไปสนุกเปล่า');
+        $result = AI::Sentiment2('ทำดีมากๆ');
+        $expected_result = 'Positive';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function testSentiment_Positive(): void
+    {
+        $result = AI::Sentiment3('ไปสนุกเปล่า');
         $expected_result = 'Positive';
         $this->assertEquals($expected_result, $result);
     }
 
  	public function testSentiment_Negative(): void
     {
-        $result = AI::Sentiment('โธ่ไอสัส');
+        $result = AI::Sentiment4('โธ่ไอสัส');
         $expected_result = 'Negative';
         $this->assertEquals($expected_result, $result);
     }
 
     public function testSentiment_Negative(): void
     {
-        $result = AI::Sentiment('ว่าไงคุณดอกทอง');
+        $result = AI::Sentiment5('ว่าไงคุณดอกทอง');
         $expected_result = 'Negative';
         $this->assertEquals($expected_result, $result);
     }
 
      public function testRudeWords_เหี้ย(): void
     {
-        $result = AI::Sentiment('ไอเหี้ยเอ้ย');
+        $result = AI::Sentiment6('ไอเหี้ยเอ้ย');
         $expected_result = 'เหี้ย';
         $this->assertEquals($expected_result, $result);
     }
 
      public function testRudeWords_fuck(): void
     {
-        $result = AI::Sentiment('fuckyou');
+        $result = AI::Sentiment7('fuckyou');
         $expected_result = 'fuck';
         $this->assertEquals($expected_result, $result);
     }
